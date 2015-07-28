20b931e8f79202c45f10c61b697c7a695d11b01a
WeekendLu <weekendlu@icloud.com>
2015-07-29 04:10:39 +0800

    Update guess_number_game.py

--- guess_number_game.py
+++ guess_number_game.py
@@ -2,7 +2,7 @@ __author__ = 'weekendlu'
 #coding=utf-8
 import random
 
-def guessnumber(min_num=0, max_num=100):
+def guessnumber(max_num=100, min_num=0):
 
 	#min_num = int(raw_input('input the min number:'))
 	#max_num = int(raw_input('input the max number:'))
@@ -15,9 +15,11 @@ def guessnumber(min_num=0, max_num=100):
 
 	while y != x:
 		if y >= max_num:
-			print 'holy fuck!', min_num, '-', max_num,
+			print 'What the hell you doing???'
+			print min_num, '-', max_num,
 		elif y <= min_num:
-			print 'holy shit!', min_num, '-', max_num,
+			print 'What are you fucking doing???'
+			print min_num, '-', max_num,
 		if x < y < max_num:
 			max_num = y
 			print min_num, '-', max_num,
